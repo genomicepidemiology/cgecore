24b0c57c0246bcea0dad631ff96810c11f863bc8
Martin Christen Frølund Thomsen <mcft@cbs.dtu.dk>
2018-01-17 14:27:19 +0100

    Tidy up

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from setuptools import setup, find_packages
 
 with open("README.md", 'r') as f:
@@ -12,8 +13,8 @@ setup(
    name='cgecore',
    version=version,
    description='Center for Genomic Epidemiology Core Module',
-   license="Apache License, Version 2.0",
    long_description=long_description,
+   license="Apache License, Version 2.0",
    author='Martin Thomsen',
    author_email='mcft@bioinformatics.dtu.dk',
    url="https://bitbucket.org/genomicepidemiology/cge_core_module",
